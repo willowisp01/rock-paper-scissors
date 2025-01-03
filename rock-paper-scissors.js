dc1315ede93a07c4397c2428bd835d0d8154cbcf
willowisp01 <132592621+willowisp01@users.noreply.github.com>
2025-01-03 12:25:41 +0800

    Add human choice function
    Function returns the strings rock, paper, or scissors when the shorthand
    r, p, or s is entered.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -10,6 +10,20 @@ function getComputerChoice() {
     }
 }
 
+function getHumanChoice() { 
+    let choice = prompt("Please select either r, p, or s."); 
+    switch(choice) {
+        case "r": 
+            return "rock";
+        case "p":
+            return "paper";
+        case "s":
+            return "scissors";
+        default: 
+            return undefined;
+    }
+}
+
 for (let i = 0; i < 100; i++) {
     console.log(getComputerChoice());
 }
